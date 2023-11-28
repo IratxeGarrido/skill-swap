1b7eeedb5900c347ca96c66eb46bec6bbd0c0f4d
Iratxe Garrido <iratxe.ggarrido@gmail.com>
2023-11-28 16:36:11 +0100

    User gets redirected to index at login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource_or_scope)
-    new_profile_path
+    @profile = Profile.where("user_id = #{current_user.id}")
+    if @profile.empty?
+      new_profile_path
+    else
+      profiles_path
+    end
   end
 end
