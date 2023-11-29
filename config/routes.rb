29d1f09de14e3c177708d4b478f1d888e2b8c2da
JamieBorg <jamienborg@gmail.com>
2023-11-29 11:55:24 +0100

    added editpage+function

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   #root "profiles#index"
   root "profiles#index"
 
-  resources :profiles, only: %i[index show new create] do
+  resources :profiles, only: %i[index show new create edit update] do
     resources :offers, only: %i[new create]
   end
   resources :offers, only: %i[edit update destroy]
